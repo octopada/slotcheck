b396562c5f2eac5cb1477f276f5bb2de22fe36e7
octopada <gogollian@gmail.com>
2021-06-23 12:59:38 +0530

    Got course name set

--- myparser.py
+++ myparser.py
@@ -2,14 +2,33 @@ from html.parser import HTMLParser
 
 
 class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.recording = False
+        self.courses = set()
+
     def error(self, message):
         pass
 
     def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:", tag)
+        if tag != 'td':
+            return
+        if self.recording:
+            self.recording = True
+            return
+        for name, value in attrs:
+            if name == 'class' and value == 'subjectNumberColumnValue nowrap ':
+                break
+            else:
+                return
+        self.recording = True
 
     def handle_endtag(self, tag):
-        print("Encountered an end tag :", tag)
+        if tag == 'td' and self.recording:
+            self.recording = False
 
     def handle_data(self, data):
-        print("Encountered some data  :", data)
+        if self.recording:
+            course_name = str(data).strip()
+            if len(course_name) != 0:
+                self.courses.add(course_name)
--- poll.py
+++ poll.py
@@ -11,4 +11,5 @@ def poll_site(url):
     response = get_response(url)
     parser = MyHTMLParser()
     parser.feed(response.text)
+    print(parser.courses)
     return response
